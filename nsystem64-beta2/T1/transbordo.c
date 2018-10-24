b79904f6af1cdcf56bc45eb4e126c56108b85684
rs077 <rodrigo.soria@ing.uchile.cl>
2018-10-24 15:50:27 -0300

    tarea terminada

--- nsystem64-beta2/T1/transbordo.c
+++ nsystem64-beta2/T1/transbordo.c
@@ -1,157 +1,109 @@
 #include <nSystem.h>
 #include "fifoqueues.h"
-#include <stdio.h>
+#include "transbordo.h"
 
 typedef struct {
-  nMonitor m;
-  FifoQueue q;
-  int *shipsarray;
-  int ships;
-} ShipControl;
+  int idFerry; // identificador del ferry
+} Ferry; // estructura para el ferry
 
 typedef struct {
-  int id;
-  nCondition w;
-} Vehicle;
+  nMonitor m; // monitor general
+  nCondition waitPargua; // condicion para vehiculos en espera en Pargua
+  FifoQueue colaVehiculosPargua; // cola para vehiculos en Pargua
+  FifoQueue colaFerrysPargua; // cola para ferrys en Pargua
+  nCondition waitChacao; // condicion para vehiculos en espera en Chacao
+  FifoQueue colaVehiculosChacao; // cola para vehiculos en Chacao
+  FifoQueue colaFerrysChacao; // cola para ferrys en Chacao
+} FerryCtrl; // estructura para el control de ferrys
 
-int nships;
+FerryCtrl *fctrl;
 
-ShipControl *makeSControl(int n) {
-    ShipControl *sc = (ShipControl *)nMalloc(sizeof(*sc));
-    sc->m= nMakeMonitor();
-    sc->q= MakeFifoQueue();
-    sc->shipsarray = (int *)nMalloc(sizeof(int)*nships);
-    sc->ships= n;
-    return sc;
+FerryCtrl *makeFerryCtrl() { // funcion para crear la estructura control de ferrys
+	FerryCtrl *f = nMalloc(sizeof(FerryCtrl)); // se pide espacio en memoria para crear la estructura
+	(*f).m = nMakeMonitor(); // se crear el monitor
+	(*f).waitPargua = nMakeCondition((*f).m); // se crea la condicion de espera en Pargua
+	(*f).colaVehiculosPargua = MakeFifoQueue(); // se crea la cola para los vehiculos en Pargua
+	(*f).colaFerrysPargua = MakeFifoQueue(); // se crea la cola para los ferrys en Pargua
+	(*f).waitChacao = nMakeCondition((*f).m); // se crea la condicion de espera en Chacao
+	(*f).colaVehiculosChacao = MakeFifoQueue(); // se crea la cola para los vehiculos en Chacao
+	(*f).colaFerrysChacao = MakeFifoQueue(); // se crea la cola para los ferrys en Chacao
+	return f; 
 }
 
-ShipControl *scPargua;
-ShipControl *scChacao;
-
-void inicializar(int p) {
-  nships = p;
-  scPargua = makeSControl(nships);
-  scChacao = makeSControl(0);
-  // Inicializa el array de 1's en Pargua para los barcos
-  for (int i = 0; i < p; i++) {
-    scPargua->shipsarray[i] = 1;
-    scChacao->shipsarray[i] = 0;
+void inicializar(int p) { // funcion que inicializa y encola los ferrys a la cola de ferrys de Pargua
+  fctrl = makeFerryCtrl(); // se crea la estructura de control de ferrys
+  for (int i = 0; i < p; i++) { 
+    Ferry *ferry = nMalloc(sizeof(ferry));
+    (*ferry).idFerry = i; // se asignan numeros con valores que van de 0 a (p-1) a cada ferry
+    PutObj((*fctrl).colaFerrysPargua, ferry); // se encolan los ferrys en Pargua
   }
 }
 
 void finalizar() {
-  nFree(scPargua);
-  nFree(scChacao);
-}
-
-void esperarBarco(ShipControl *sc, int v) {
-  Vehicle vehicle;
-  vehicle.id = v;
-  vehicle.w = nMakeCondition(sc->m);
-  PutObj(sc->q, &vehicle);
-  nWaitCondition(vehicle.w);
-  nDestroyCondition(vehicle.w);
-}
-
-void llamarBarco(ShipControl *scOrigen, ShipControl *scDestino, void (*haciaDestino)(), int v) {
-  // Solo llamará un barco si no hay fila de espera y estan todos los barcos en el origen
-  if (EmptyFifoQueue(scOrigen->q) && scOrigen->ships == nships) {
-    for (int i = 0; i < nships; i++) {
-      if (scOrigen->shipsarray[i]) {
-        scOrigen->shipsarray[i] = 0;
-        scOrigen->ships--;
-        haciaDestino(i,-1);
-        scDestino->shipsarray[i] = 1;
-        scDestino->ships++;
-        return;
-      }
-    }
-  }
-  // Si no, el vehiculo debe agregarse a la cola
-  else
-    esperarBarco(scDestino, v);
-}
-
-void notificarCola(ShipControl *sc) {
-  Vehicle *vehicle= (Vehicle*)GetObj(sc->q);
-  if (vehicle == NULL)
-    return;
-  if (sc->ships > 0)
-    nSignalCondition(vehicle->w);
-  else {
-    PushObj(sc->q, vehicle);
-  }
-}
-
-void notificarLlegada(ShipControl *sc) {
-  Vehicle *vehicle= (Vehicle*)GetObj(sc->q);
-  if (vehicle == NULL)
-    return;
-  nSignalCondition(vehicle->w);
+  nFree(fctrl); // se libera el espacio pedido por la estructura
 }
 
 void transbordoAChacao(int v) {
-  nEnter(scPargua->m);
-  // En caso de que haya fila, debe esperar
-  if (!EmptyFifoQueue(scPargua->q))
-    esperarBarco(scPargua, v);
-  // Si no hay barcos, se llama un barco de Chacao a Pargua
-  while (!scPargua->ships)
-    llamarBarco(scChacao, scPargua, haciaPargua, v);
-  // Revisa los barcos disponibles para iniciar el viaje
-  for (int i = 0; i < nships; i++) {
-    if (scPargua->shipsarray[i]) {
-      scPargua->shipsarray[i] = 0;
-      scPargua->ships--;
-      // Hace la notificacion al que le toca en la cola
-      notificarCola(scPargua);
-      nExit(scPargua->m);
-      // Viaje largo a Chacao
-      haciaChacao(i,v);
-      nEnter(scChacao->m);
-      scChacao->shipsarray[i] = 1;
-      scChacao->ships++;
-      // Hace la notificacion al que le toca en la cola en Chacao y despues
-      // a Pargua por si se cumple la condicion para llamar barcos
-      notificarCola(scChacao);
-      nExit(scChacao->m);
-      nEnter(scPargua->m);
-      notificarLlegada(scPargua);
-      nExit(scPargua->m);
-      return;
+  nEnter((*fctrl).m); // se entra a la seccion critica
+  // si la cola de ferrys en Pargua esta vacia o la cola de vehiculos en Pargua no lo esta
+  if (EmptyFifoQueue((*fctrl).colaFerrysPargua) || !EmptyFifoQueue((*fctrl).colaVehiculosChacao)) {
+  	// se pone el vehiculo a la cola 
+  	PutObj((*fctrl).colaVehiculosPargua, &v);
+  }
+  // mientras no haya ferrys en Pargua
+  while (EmptyFifoQueue((*fctrl).colaFerrysPargua)) {
+  	// si hay ferrys en Chacao y no hay vehiculos en esperando, se manda un ferry vacio a Pargua
+    if(!EmptyFifoQueue((*fctrl).colaFerrysChacao) && EmptyFifoQueue((*fctrl).colaVehiculosChacao)) {
+    	Ferry *ferry = GetObj(fctrl->colaFerrysChacao); // se desencola el ferry de Chacao
+    	haciaPargua(ferry->idFerry,-1); // se envia el ferry vacio 
+    	PutObj((*fctrl).colaFerrysPargua, ferry); // se encola el ferry en Pargua
+    	nSignalCondition((*fctrl).waitPargua); // se avisa que llego un ferry a Pargua a los vehiculos
+    }
+    else { // si no
+    	nWaitCondition((*fctrl).waitPargua); // el vehiculo se pone en espera
     }
   }
+  // si hay ferrys en Pargua y vehiculos en la cola, hace transbordo a Chacao
+  Ferry *ferry = GetObj(fctrl->colaFerrysPargua);
+  nExit((*fctrl).m); // fin seccion critica
+  haciaChacao(ferry->idFerry,v); // se envia el ferry con un vehiculo
+  nEnter((*fctrl).m); // inicio seccion critica
+  PutObj((*fctrl).colaFerrysChacao, ferry);  // el ferry llega a Chacao y se encola
+  nSignalCondition((*fctrl).waitChacao); // se avisa que llego un ferry a Chacao a los vehiculos
+  DeleteObj((*fctrl).colaVehiculosPargua, &v); // se elimina el vehiculo de la cola
+  nExit((*fctrl).m); // fin seccion critica
+  return;
 }
 
 void transbordoAPargua(int v) {
-  nEnter(scChacao->m);
-  // En caso de que haya fila, debe esperar
-  if (!EmptyFifoQueue(scChacao->q))
-    esperarBarco(scChacao, v);
-  // Si no hay barcos, se llama un barco de Pargua a Chacao
-  while (!scChacao->ships)
-      llamarBarco(scPargua, scChacao, haciaChacao, v);
-  // Revisa los barcos disponibles para iniciar el viaje
-  for (int i = 0; i < nships; i++) {
-    if (scChacao->shipsarray[i]) {
-      scChacao->shipsarray[i] = 0;
-      scChacao->ships--;
-      notificarCola(scChacao);
-      nExit(scChacao->m);
-      // Hace la notificacion al que le toca en la cola en Chacao y despues a Pargua
-      // Viaje largo a Pargua
-      haciaPargua(i,v);
-      nEnter(scPargua->m);
-      scPargua->shipsarray[i] = 1;
-      scPargua->ships++;
-      // Hace la notificacion al que le toca en la cola en Pargua y despues
-      // a Chacao por si se cumple la condicion para llamar barcos
-      notificarCola(scPargua);
-      nExit(scPargua->m);
-      nEnter(scChacao->m);
-      notificarLlegada(scChacao);
-      nExit(scChacao->m);
-      return;
+  nEnter((*fctrl).m); // se entra a la seccion critica
+  // si la cola de ferrys en Chacao esta vacia o la cola de vehiculos en Chacao no lo esta
+  if (EmptyFifoQueue((*fctrl).colaFerrysChacao) || !EmptyFifoQueue((*fctrl).colaVehiculosChacao)) {
+  	// se pone el vehiculo a la cola 
+  	PutObj((*fctrl).colaVehiculosChacao, &v);
+  }
+  // mientras no haya ferrys en Pargua
+  while (EmptyFifoQueue((*fctrl).colaFerrysChacao)){
+  	// si hay ferrys en Pargua y no hay vehiculos en esperando, se manda un ferry vacio a Chacao
+  	if(!EmptyFifoQueue((*fctrl).colaFerrysPargua) && EmptyFifoQueue((*fctrl).colaVehiculosPargua)) {
+    	Ferry *ferry = GetObj((*fctrl).colaFerrysPargua); // se desencola el ferry de Pargua
+    	haciaChacao(ferry->idFerry,-1);
+    	PutObj((*fctrl).colaFerrysChacao, ferry); // se encola el ferry en Chacao
+    	nSignalCondition((*fctrl).waitChacao); // se avisa que llego un ferry a Chacao a los vehiculos
+    }
+
+    else{ // sino
+    	nWaitCondition((*fctrl).waitChacao); // el vehiculo se pone en espera
     }
   }
+  // si hay ferrys en Pargua y vehiculos en la cola, hace transbordo a Pargua
+  Ferry *ferry = GetObj(fctrl->colaFerrysChacao);
+  nExit((*fctrl).m); // fin de seccion critica
+  haciaPargua(ferry->idFerry,v); // se envia el ferry con un vehiculo
+  nEnter((*fctrl).m); // inicio de seccion critica
+  PutObj((*fctrl).colaFerrysPargua, ferry);  // el ferry llega a Pargua y se encola
+  nSignalCondition((*fctrl).waitPargua); // se avisa que llego un ferry a Pargua a los vehiculos
+  DeleteObj((*fctrl).colaVehiculosChacao, &v); // se elimina el vehiculo de la cola
+  nExit((*fctrl).m); // fin de seccion critica
+  return;
 }
